d2748f68321ecff16d525f64cbfac5b4a409ce27
Eduardo <panagee3@gmail.com>
2024-09-27 16:20:37 -0400

    Correção de Bugs 2 na Listagem de Contatos

--- lib/ListagemDeContatos.dart
+++ lib/ListagemDeContatos.dart
@@ -1,7 +1,6 @@
-import 'package:controle_frutas/FormulariodeCOntato.dart';
+import 'package:controle_frutas/FormulariodeContato.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'FormularioDeContato.dart';
 import 'GerenciadorDeContatos.dart';
 import 'Contato.dart'; 
 
